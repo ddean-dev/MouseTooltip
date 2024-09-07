d2349cf5787a409680547ab5bb4de2832fba3921
David Dean <david@ddean.dev>
2024-09-07 12:34:15 -0600

    refactor to prevent race condition

--- main.lua
+++ main.lua
@@ -1,3 +1,12 @@
-hooksecurefunc("GameTooltip_SetDefaultAnchor", function(s, p)
-	s:SetOwner(p, "ANCHOR_CURSOR")
-end)
+local MouseTooltip = CreateFrame("Frame")
+
+function MouseTooltip:OnEvent(event, addon)
+	if event == "ADDON_LOADED" and addon == "MouseTooltip" then
+		hooksecurefunc("GameTooltip_SetDefaultAnchor", function(s, p)
+			s:SetOwner(p, "ANCHOR_CURSOR")
+		end)
+	end
+end
+
+MouseTooltip:RegisterEvent("ADDON_LOADED")
+MouseTooltip:SetScript("OnEvent", MouseTooltip.OnEvent)
